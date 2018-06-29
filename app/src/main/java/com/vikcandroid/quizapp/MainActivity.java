f84977cfe2bc32078bc6408028423ee7b3faacbb
Vikctar <vikc.steve@gmail.com>
2018-06-29 23:51:35 +0300

    Show a message when submit is clicked.

--- app/src/main/java/com/vikcandroid/quizapp/MainActivity.java
+++ app/src/main/java/com/vikcandroid/quizapp/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup cRadioGroup;
@@ -20,19 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
     // Radio buttons
     // Release year radio buttons
-    private RadioButton twentyTen, nineteenSixty, twentyEleven;
+    private RadioButton releaseRadioButton;
 
     // Codename radio buttons
-    private RadioButton oreo, kitkat, nougat;
+    private RadioButton codenameRadioButton;
 
     // founder radio buttons
-    private RadioButton rubin, gates, trump;
+    private RadioButton founderRadioButton;
 
     // owner radio button
-    private RadioButton google, microsoft, facebook;
+    private RadioButton ownerRadioButton;
 
     // kernel radio buttons
-    private RadioButton linux, windows, unix;
+    private RadioButton kernelRadioButton;
+
+    // Git creator EditText
+    private AppCompatEditText gitCreator;
+
+    // Language checkboxes
+    private CheckBox javaCheckbox, kotlinCheckbox, swiftCheckbox, cppCheckbox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +58,47 @@ public class MainActivity extends AppCompatActivity {
         oRadioGroup = findViewById(R.id.owner_radio_group);
         kRadioGroup = findViewById(R.id.kernel_radio_group);
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        gitCreator = findViewById(R.id.creator_name_edit);
+
+        javaCheckbox = findViewById(R.id.java_checkbox);
+        kotlinCheckbox = findViewById(R.id.kotlin_checkbox);
+        swiftCheckbox = findViewById(R.id.swift_checkbox);
+        cppCheckbox = findViewById(R.id.c_checkbox);
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fab.hide();
+                int releaseId = rRadioGroup.getCheckedRadioButtonId();
+                int codenameId = cRadioGroup.getCheckedRadioButtonId();
+                int founderId = fRadioGroup.getCheckedRadioButtonId();
+                int ownerId = oRadioGroup.getCheckedRadioButtonId();
+                int kernelId = kRadioGroup.getCheckedRadioButtonId();
 
-                Snackbar.make(view, "Yaas!! You got everything correct!", Snackbar.LENGTH_LONG).show();
-                fab.show();
+                codenameRadioButton = findViewById(codenameId);
+                releaseRadioButton = findViewById(releaseId);
+                founderRadioButton = findViewById(founderId);
+                ownerRadioButton = findViewById(ownerId);
+                kernelRadioButton = findViewById(kernelId);
+
+
+                String codeName = codenameRadioButton.getText().toString();
+                String release = releaseRadioButton.getText().toString();
+                String founder = founderRadioButton.getText().toString();
+                String owner = ownerRadioButton.getText().toString();
+                String kernel = kernelRadioButton.getText().toString();
+
+                String gitCreatorString = gitCreator.getText().toString();
+
+                if (codeName.equals("Oreo") && release.equals("2010") && founder.equals("Andy Rubin")
+                        && owner.equals("Google") && kernel.equals("Linux") && gitCreatorString.equals("Linus Torvalds")
+                        && javaCheckbox.isChecked() && kotlinCheckbox.isChecked() && cppCheckbox.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Congratulations!!! You got everything correct", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "One or more answers are not correct", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -83,6 +127,12 @@ public class MainActivity extends AppCompatActivity {
             fRadioGroup.clearCheck();
             rRadioGroup.clearCheck();
             kRadioGroup.clearCheck();
+
+            gitCreator.setText("");
+            javaCheckbox.setChecked(false);
+            kotlinCheckbox.setChecked(false);
+            cppCheckbox.setChecked(false);
+            swiftCheckbox.setChecked(false);
         }
 
         return super.onOptionsItemSelected(item);
